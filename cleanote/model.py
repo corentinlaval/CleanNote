d48d3a5756da249db264ac26f6e9f2d35cc68b35
Corentin Laval <corentinlaval1@gmail.com>
2025-09-25 13:16:34 +0200

    delete optionnal

--- cleanote/model.py
+++ cleanote/model.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Dict, Optional, List
+from typing import Any, Dict, List
 import copy
 
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline

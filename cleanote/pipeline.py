9312945f7785087415c29def30c601fa059e307b
Corentin Laval <corentinlaval1@gmail.com>
2025-09-25 14:02:57 +0200

    v0.1.7

--- cleanote/pipeline.py
+++ cleanote/pipeline.py
@@ -7,15 +7,15 @@ class Pipeline:
     def apply(self):
         print("[Pipeline] Starting pipeline...")
 
-        prompt_h = "please count the number of words in the following text:"
+        prompt_h = "please give me the number of words in the following text:"
         print("[Pipeline] Start Homogenization...")
         dataset_h = self.model_h.run(self.dataset, prompt_h)
         print("[Pipeline] Homogenization completed.")
 
-        prompt_v = "please verify the following text is not empty:"
-        print("[Pipeline] Start Verification...")
-        dataset_v = self.model_v.run(dataset_h, prompt_v)
-        print("[Pipeline] Verification completed.")
+        # prompt_v = "please verify the following text is not empty:"
+        # print("[Pipeline] Start Verification...")
+        # dataset_v = self.model_v.run(dataset_h, prompt_v)
+        # print("[Pipeline] Verification completed.")
 
         print("[Pipeline] Pipeline completed.")
-        return dataset_v
+        return dataset_h
